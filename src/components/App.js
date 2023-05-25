388724deac1b4dc8f55d04e77566c0a927c8ab2d
DennyBui <dbuidmd2018@gmail.com>
2023-05-25 11:01:59 -0700

    lab complete

--- src/components/App.js
+++ src/components/App.js
@@ -3,18 +3,30 @@ import NavBar from "./NavBar";
 import Home from "./Home";
 import About from "./About";
 
+
 // pass this data down as props to the child component(s) that need it!
 import user from "../data/user";
-console.log(user);
+console.log(user)
+
 
 function App() {
+  const {name, city, color} = user
   return (
     <div>
       <NavBar />
-      <Home />
-      <About />
+      <Home
+      name={name}
+      city={city}
+      color={color}
+      />
+      <About
+      bio={user.bio}
+      github={user.links.github}
+      linkedin={user.links.linkedin}
+      />
     </div>
   );
 }
+
 
 export default App;
